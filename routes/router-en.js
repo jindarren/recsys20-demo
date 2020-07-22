56feb3e021bb83ba8dbc91d7dd5e8d3d3be4895e
wanlingcai <382970614@qq.com>
2020-07-22 19:54:39 +0800

    Update router-en.js

--- routes/router-en.js
+++ routes/router-en.js
@@ -37,8 +37,8 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new SpotifyStrategy({
         clientID: appKey,
         clientSecret: appSecret,
-        // callbackURL: 'http://music-bot.top:3000/callback'
-        callbackURL: 'http://localhost:3000/callback'
+        callbackURL: 'http://music-bot.top:3000/callback'
+        // callbackURL: 'http://localhost:3000/callback'
     },
     function(accessToken, refreshToken, profile, done) {
         // asynchronous verification, for effect...
